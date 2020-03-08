314837739c1d9f8dc8d956e81174b75f3ec04e9f
khuzha <mr.khuzha@gmail.com>
2020-03-08 15:56:52 +0300

    kek

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 const createBot = require('./lib/createBot')
 const createInstance = require('./lib/createInstance')   
 const launchBots = require('./lib/launchBots')
+=======
+import createBot from './lib/createBot'
+import createInstance from './lib/createInstance'   
+import launchBots from './lib/launchBots'
+>>>>>>> parent of 3afb5c2... refractore: make import ... from instead of const ... require
 
 launchBots()
